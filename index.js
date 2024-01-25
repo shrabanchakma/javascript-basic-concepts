ed8eca855fac72aed4865fdac49b75d38f199cb8
Shraban Chakma <shrabanchakma328@gmail.com>
2024-01-25 22:45:23 +0600

    added closure topic

--- index.js
+++ index.js
@@ -1219,3 +1219,50 @@ document.getElementById("p3").textContent = `Am i a student? ${isStudent}`;*/
 // console.log(dayOfWeek);
 
 // ==========================================================================VIDEO40
+
+// #closure  -a function defined inside of another function, the inner function has access to the variables and scope of the outer function
+// Allow for private variables and state maintenance
+
+// function createCounter() {
+//   let count = 0;
+
+//   function increment() {
+//     count++;
+//     console.log(`count increased to ${count}`);
+//   }
+
+//   function getCount() {
+//     return count;
+//   }
+//   return { increment, getCount };
+// }
+
+// const counter = createCounter();
+// counter.increment();
+// counter.increment();
+// console.log(`the current count is: ${counter.getCount()}`);
+
+function createGame() {
+  let score = 0;
+
+  function increaseScore(points) {
+    score += points;
+    console.log(`+${points}pts`);
+  }
+  function decreaseScore(points) {
+    score -= points;
+    console.log(`-${points}pts`);
+  }
+  function getScore() {
+    return score;
+  }
+
+  return { increaseScore, decreaseScore, getScore };
+}
+
+const game = createGame();
+
+game.increaseScore(5);
+game.increaseScore(5);
+game.decreaseScore(3);
+console.log(`current score is ${game.getScore()}`);
